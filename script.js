3a1a9149103bd3a2c51223be441340b3cafa3e92
EmmaTheVibe <peteremmanuel316@gmail.com>
2023-06-16 15:11:55 +0100

    test2

--- script.js
+++ script.js
@@ -51,29 +51,27 @@ buttons.forEach(active => {
         active.classList.add('clicked')
     })
 })
-function receive(){
-    // const img1 = document.querySelector(".state")
-    // img1.setAttribute('src', "images/screenreceive.svg") 
+// function receive(){
     
-    const transferText = document.querySelector(".heading")
-    transferText.innerText = "Receive Money"
+//     const transferText = document.querySelector(".heading")
+//     transferText.innerText = "Receive Money"
     
-    const transferPrg = document.querySelector(".prg")
-    transferPrg.innerText = "Say goodbye to complicated payment methods and delays. Now, you can easily receive money instantly from anyone, anywhere. Simplify your payment collection with YurWallet using a unique account ID created."
-}
+//     const transferPrg = document.querySelector(".prg")
+//     transferPrg.innerText = "Say goodbye to complicated payment methods and delays. Now, you can easily receive money instantly from anyone, anywhere. Simplify your payment collection with YurWallet using a unique account ID created."
+// }
 
-function transfer(){
-    const transferText = document.querySelector(".heading")
-    transferText.innerText = "Transfer Money"
+// function transfer(){
+//     const transferText = document.querySelector(".heading")
+//     transferText.innerText = "Transfer Money"
     
-    const transferPrg = document.querySelector(".prg")
-    transferPrg.innerText = "No more worries about delayed or failed transactions. YurWallet enables you to send money effortlessly to any bank of your choice. With just a few taps, you can transfer funds quickly and safely, allowing you to fulfil your financial obligations without any hassles. Enjoy peace of mind knowing that your transactions are completed smoothly and promptly."
-}
+//     const transferPrg = document.querySelector(".prg")
+//     transferPrg.innerText = "No more worries about delayed or failed transactions. YurWallet enables you to send money effortlessly to any bank of your choice. With just a few taps, you can transfer funds quickly and safely, allowing you to fulfil your financial obligations without any hassles. Enjoy peace of mind knowing that your transactions are completed smoothly and promptly."
+// }
 
-function fund(){
-    const transferText = document.querySelector(".heading")
-    transferText.innerText = "Fund Wallet"
+// function fund(){
+//     const transferText = document.querySelector(".heading")
+//     transferText.innerText = "Fund Wallet"
     
-    const transferPrg = document.querySelector(".prg")
-    transferPrg.innerText = "Instantly top-up your wallet using your card, USSD codes, or a simple bank transfer. YurWallet ensures that your funds are readily available whenever needed, enabling you to make seamless payments and enjoy uninterrupted services."
-}
+//     const transferPrg = document.querySelector(".prg")
+//     transferPrg.innerText = "Instantly top-up your wallet using your card, USSD codes, or a simple bank transfer. YurWallet ensures that your funds are readily available whenever needed, enabling you to make seamless payments and enjoy uninterrupted services."
+// }
